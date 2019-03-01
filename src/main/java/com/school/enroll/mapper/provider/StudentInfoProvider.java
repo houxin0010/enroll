d93f24157acfc3cbac71f26be4b841532bdd72f2
houxin <183188574@qq.com>
2019-03-01 18:40:53 +0800

    表格查询

--- src/main/java/com/school/enroll/mapper/provider/StudentInfoProvider.java
+++ src/main/java/com/school/enroll/mapper/provider/StudentInfoProvider.java
@@ -1,12 +1,29 @@
 package com.school.enroll.mapper.provider;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.jdbc.SQL;
+import org.springframework.util.StringUtils;
 
 public class StudentInfoProvider {
 
-    public String findByFieldSql(){
-        return new SQL(){{
-
+    public String findByFieldSql(@Param("name") String name, @Param("status") String status,
+                                 @Param("startTime") String startTime, @Param("endTime") String endTime) {
+        return new SQL() {{
+            SELECT("*");
+            FROM("student_info");
+            if (!StringUtils.isEmpty(name)) {
+                WHERE("name = #{name}");
+            }
+            if (!StringUtils.isEmpty(status)) {
+                WHERE("status = #{status}");
+            }
+            if (!StringUtils.isEmpty(startTime)) {
+                WHERE("create_time >= #{startTime}");
+            }
+            if (!StringUtils.isEmpty(endTime)) {
+                WHERE("create_time < #{endTime}");
+            }
+            ORDER_BY("create_time DESC");
         }}.toString();
     }
 }
--- src/test/java/com/school/enroll/EnrollApplicationTests.java
+++ src/test/java/com/school/enroll/EnrollApplicationTests.java
@@ -1,16 +1,31 @@
 package com.school.enroll;
 
+import com.alibaba.fastjson.JSON;
+import com.school.enroll.entity.StudentInfo;
+import com.school.enroll.mapper.StudentInfoMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EnrollApplicationTests {
 
+    @Autowired
+    private StudentInfoMapper studentInfoMapper;
+
     @Test
     public void contextLoads() {
+//        List<StudentInfo> studentInfos = studentInfoMapper.findByField("学生1", "", "", "");
+
+        log.info("result = {} - {}", String.valueOf(System.currentTimeMillis()), new Date().getTime());
     }
 
 }
