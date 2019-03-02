b248bc598bba3665cd08d08ca7647f89a23081a1
houxin <183188574@qq.com>
2019-03-02 18:23:42 +0800

    公共js

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,18 @@
+$('select.dropdown').dropdown();
+
+$("div.calendar").calendar({
+    type: 'date',
+    formatter: { // 自定义日期的格式
+        date: function (date, settings) {
+            if (!date) return '';
+            let year = date.getFullYear();
+            let month = date.getMonth() + 1;
+            let day = date.getDate();
+
+            month = month < 10 ? '0' + month : month;
+            day = day < 10 ? '0' + day : day;
+
+            return year + '-' + month + '-' + day;
+        }
+    }
+});
