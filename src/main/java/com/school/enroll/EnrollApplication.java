5d59a60e9240a82ad7dc45a91f20906e2f6ae748
houxin <183188574@qq.com>
2019-03-19 23:50:09 +0800

    报名系统

--- src/main/java/com/school/enroll/EnrollApplication.java
+++ src/main/java/com/school/enroll/EnrollApplication.java
@@ -1,11 +1,9 @@
 package com.school.enroll;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.school.enroll.mapper")
 public class EnrollApplication {
 
     public static void main(String[] args) {
